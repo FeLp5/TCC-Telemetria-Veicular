f0394a2e3cc157e53b3182d41fc079e5fec81054
decsouza <decsouza2008@gmail.com>
2022-02-04 18:13:45 -0300

    modificado GPS

---
+++ SDCARDV6/build/default/production/bibliotecas/pff.p1.d
@@ -0,0 +1,7 @@
+ build/default/production/bibliotecas/pff.d  \
+ build/default/production/bibliotecas/pff.p1:  \
+ bibliotecas/pff.c  \
+bibliotecas/pff.h  \
+bibliotecas/diskio.h  \
+bibliotecas/integer.h  \
+bibliotecas/pffconf.h 
---
+++ SDCARDV6/build/default/production/main.p1.d
@@ -0,0 +1,18 @@
+ build/default/production/main.d  \
+ build/default/production/main.p1:  \
+ main.c  \
+bibliotecas/adc.h  \
+bibliotecas/pff.h  \
+bibliotecas/uart.h  \
+bibliotecas/diskio.h  \
+bibliotecas/SDCard.h  \
+main.h  \
+bibliotecas/integer.h  \
+bibliotecas/SHRC.h  \
+bibliotecas/chaves.h  \
+bibliotecas/config.h  \
+bibliotecas/SPI.h  \
+bibliotecas/display_lcd.h  \
+bibliotecas/pffconf.h  \
+bibliotecas/GPS.h  \
+bibliotecas/hardware.h 
