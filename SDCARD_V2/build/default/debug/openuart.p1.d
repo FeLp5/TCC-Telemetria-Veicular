ac382e456107822897fc4d10caf8d898f54469cd
Felipe <69984907+FeLp5@users.noreply.github.com>
2022-04-02 12:30:32 -0300

    Delete SDCARD_V2 directory

--- SDCARD_V2/build/default/debug/openuart.p1.d
+++
@@ -1,3 +0,0 @@
- build/default/debug/openuart.d  \
- build/default/debug/openuart.p1:  \
- openuart.c 
--- SDCARD_V2/build/default/production/uartdata.p1.d
+++
@@ -1,3 +0,0 @@
- build/default/production/uartdata.d  \
- build/default/production/uartdata.p1:  \
- uartdata.c 
