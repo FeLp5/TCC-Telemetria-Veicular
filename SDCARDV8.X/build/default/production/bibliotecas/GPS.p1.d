5784901359f8c70f944fbda6b0463b6c32a43048
Felipe <69984907+FeLp5@users.noreply.github.com>
2022-04-02 12:29:03 -0300

    Delete SDCARDV8.X directory

--- SDCARDV8.X/build/default/production/bibliotecas/GPS.p1.d
+++
@@ -1,6 +0,0 @@
- build/default/production/bibliotecas/GPS.d  \
- build/default/production/bibliotecas/GPS.p1:  \
- bibliotecas/GPS.c  \
-bibliotecas/display_lcd.h  \
-bibliotecas/GPS.h  \
-bibliotecas/hardware.h 
