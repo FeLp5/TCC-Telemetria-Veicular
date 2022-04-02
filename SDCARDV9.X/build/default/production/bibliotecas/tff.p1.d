286693a9f2e9b1e52c953b8d653a3b555706103a
Felipe <69984907+FeLp5@users.noreply.github.com>
2022-04-02 12:33:46 -0300

    Delete SDCARDV9.X directory

--- SDCARDV9.X/build/default/production/bibliotecas/tff.p1.d
+++
@@ -1,8 +0,0 @@
- build/default/production/bibliotecas/tff.d  \
- build/default/production/bibliotecas/tff.p1:  \
- bibliotecas/tff.c  \
-bibliotecas/diskio.h  \
-bibliotecas/tff.h  \
-bibliotecas/integer.h  \
-bibliotecas/display_lcd.h  \
-bibliotecas/hardware.h 
