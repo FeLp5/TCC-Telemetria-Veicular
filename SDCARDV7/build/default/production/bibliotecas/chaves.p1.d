33a7950e14aeccabbd394fd714cdde02e7240bf7
Felipe <69984907+FeLp5@users.noreply.github.com>
2022-04-02 12:29:35 -0300

    Delete SDCARDV7 directory

--- SDCARDV7/build/default/production/bibliotecas/chaves.p1.d
+++
@@ -1,6 +0,0 @@
- build/default/production/bibliotecas/chaves.d  \
- build/default/production/bibliotecas/chaves.p1:  \
- bibliotecas/chaves.c  \
-bibliotecas/../main.h  \
-bibliotecas/display_lcd.h  \
-bibliotecas/hardware.h 
