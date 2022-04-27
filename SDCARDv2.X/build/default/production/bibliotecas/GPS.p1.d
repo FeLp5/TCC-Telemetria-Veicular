6fa9f55cba4dda9e8b0d1cf5705ad2e41f34827b
FeLp5 <empreenda165@gmail.com>
2022-04-27 15:40:16 -0300

    Removendo codigo da versao anterior do compilador

--- SDCARDv2.X/build/default/production/bibliotecas/GPS.p1.d
+++
@@ -1,8 +0,0 @@
- build/default/production/bibliotecas/GPS.d  \
- build/default/production/bibliotecas/GPS.p1:  \
- bibliotecas/GPS.c  \
-bibliotecas/diskio.h  \
-bibliotecas/SDCard.h  \
-bibliotecas/integer.h  \
-bibliotecas/GPS.h  \
-bibliotecas/hardware.h 
